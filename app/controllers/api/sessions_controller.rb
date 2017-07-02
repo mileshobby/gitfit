7dfe5f825d15e9336511ac3d71d65e9e9bd250d1
mileshobby <mhobby16@gmail.com>
2017-07-01 21:31:48 -0700

    finish user and session api controllers

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,7 +1,20 @@
 class Api::SessionsController < ApplicationController
   def create
+    @user = User.find_by_credentials(params[:user][:username], params[:user][:password])
+    if @user
+      login(@user)
+      render json: @user
+    else
+      render json: ["Invalid Login Credentials"], status: 404
+    end
   end
 
   def destroy
+    if logged_in?
+      log_out
+      render json: {}
+    else
+      render json: ["Cannot log out without being signed in"], status: 404
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
-  namespace :api do
-    get 'sessions/create'
-  end
-
-  namespace :api do
-    get 'sessions/destroy'
+  namespace :api, { format: :json } do
+    resources :users, only: [:create]
+    resource :session, only: [:create, :destroy]
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
