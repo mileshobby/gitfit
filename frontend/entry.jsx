1664bfd4324ef0a0105ed195a4abdbf86f78bcf4
mileshobby <mhobby16@gmail.com>
2017-07-01 22:32:31 -0700

    set up store in entry file

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './root';
+import configureStore from './store/store';
 
 document.addEventListener('DOMContentLoaded', () => {
+  let store;
+  if(window.currentUser){
+    const preloadedState = {currentUser: window.currentUser};
+    store = configureStore(preloadedState);
+    delete window.currentUser;
+  }
+  else{
+    store = configureStore();
+  }
+  window.store = store;
   ReactDOM.render(<Root />, document.getElementById('main'));
 });
