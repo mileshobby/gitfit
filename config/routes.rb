c2ab2c7a8e30d8880c485d14fb16511f46050de3
mileshobby <mhobby16@gmail.com>
2017-07-01 21:18:00 -0700

    add user model with auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  namespace :api do
+    get 'sessions/create'
+  end
+
+  namespace :api do
+    get 'sessions/destroy'
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
